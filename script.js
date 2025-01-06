4b1a759eaa3f93ef57813db9d1f8cf6120efb779
RSchmauk <Rodolfo.Schmauk@gmail.com>
2025-01-06 12:13:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,29 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const video = document.getElementById('preview');
-    const player = document.getElementById('player');
-    const playButton = document.getElementById('play');
-    const pauseButton = document.getElementById('pause');
+import QrScanner from 'https://cdnjs.cloudflare.com/ajax/libs/qr-scanner/1.4.1/qr-scanner.umd.min.js';
 
-    const html5QrCode = new Html5Qrcode("preview");
+const video = document.getElementById('preview');
+const player = document.getElementById('player');
+const playButton = document.getElementById('play');
+const pauseButton = document.getElementById('pause');
 
-    function handleQRCode(decodedText) {
-        if (decodedText.startsWith('https://music.youtube.com/watch')) {
-            player.src = decodedText.replace('music.youtube.com', 'www.youtube.com') + '&autoplay=1';
-            player.style.display = 'block';
-        } else {
-            alert('Por favor, escanea un enlace válido de YouTube Music.');
-        }
+QrScanner.WORKER_PATH = 'https://cdnjs.cloudflare.com/ajax/libs/qr-scanner/1.4.1/qr-scanner-worker.min.js';
+
+const scanner = new QrScanner(video, result => handleQRCode(result));
+
+scanner.start();
+
+function handleQRCode(result) {
+    if (result.startsWith('https://music.youtube.com/watch')) {
+        player.src = result.replace('music.youtube.com', 'www.youtube.com') + '&autoplay=1';
+        player.style.display = 'block';
+    } else {
+        alert('Por favor, escanea un enlace válido de YouTube Music.');
     }
+}
 
-    html5QrCode.start(
-        { facingMode: "environment" }, 
-        {
-            fps: 10, 
-            qrbox: { width: 250, height: 250 }
-        },
-        (decodedText, decodedResult) => {
-            handleQRCode(decodedText);
-        }
-    ).catch(err => {
-        console.log(`Error: ${err}`);
-    });
+playButton.addEventListener('click', () => {
+    player.contentWindow.postMessage('{"event":"command","func":"playVideo","args":""}', '*');
+});
 
-    playButton.addEventListener('click', () => {
-        player.contentWindow.postMessage('{"event":"command","func":"playVideo","args":""}', '*');
-    });
-
-    pauseButton.addEventListener('click', () => {
-        player.contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
-    });
+pauseButton.addEventListener('click', () => {
+    player.contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
 });
